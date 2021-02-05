e3f1044da73d709364ec4d8f4502797de1d572be
StonedDawg <wazzlup@hotmail.com>
2021-02-05 20:54:10 +0700

    sfds

--- src/src/settings.h
+++ src/src/settings.h
@@ -115,16 +115,4 @@ SOFTWARE.
 #define DELAY_10_HZ (1000 / 10)
 #define DELAY_5_HZ (1000 / 5)
 
-#define BUTTON_DEBOUNCE_DELAY 300
-
-
-#define VRX_LED0_TOGGLE digitalWrite(VRX_LED0,!digitalRead(VRX_LED0))
-#define  VRX_LED0_OFF   digitalWrite(VRX_LED0,LOW);
-#define  VRX_LED0_ON    digitalWrite(VRX_LED0,HIGH);
-#define VRX_LED1_TOGGLE digitalWrite(VRX_LED1,!digitalRead(VRX_LED1))
-#define  VRX_LED1_OFF   digitalWrite(VRX_LED1,LOW);
-#define  VRX_LED1_ON    digitalWrite(VRX_LED1,HIGH);
-#define VRX_LED2_TOGGLE digitalWrite(VRX_LED2,!digitalRead(VRX_LED2))
-#define  VRX_LED2_OFF   digitalWrite(VRX_LED2,LOW);
-#define  VRX_LED2_ON    digitalWrite(VRX_LED2,HIGH);
 #endif // file_defined
