e0d6bcb6f2cb211931c405dc6d43736fe5de678a
StonedDawg <wazzlup@hotmail.com>
2021-04-13 18:12:55 +0700

    set default noswitchlow to off

--- src/src/settings_eeprom.h
+++ src/src/settings_eeprom.h
@@ -111,7 +111,7 @@ const struct {
     uint8_t rssiHysteresis = 100;
     uint16_t rssiHysteresisPeriod = 5;
     uint8_t rssiInverted = 1;
-    uint8_t noSwitchOnLow = 1;
+    uint8_t noSwitchOnLow = 0;
 
 } EepromDefaults;
 
